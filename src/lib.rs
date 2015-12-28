54eb9c4a700afec2e33183ce3cb82cd03f57052e
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2015-12-29 00:25:41 +0800

    Add convert_to_bopomofo

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
-struct PreeditSlot {
-    bopomofo: u16
-}
-
-struct Bopomofo {
-    preedit_buffer: Vec<PreeditSlot>
-}
-
-impl Bopomofo {
-    fn input(self, ch: char, pos: usize) {
+fn convert_to_bopomofo(ch: char) -> Option<u16> {
+    match ch {
+        'ˊ' => Some(1),
+        'ˇ' => Some(2),
+        'ˋ' => Some(3),
+        '˙' => Some(4),
+        _ => None,
     }
+}
+
+#[test]
+fn test_convert_to_bopomofo() {
+    assert_eq!(convert_to_bopomofo('a'), None);
+    assert_eq!(convert_to_bopomofo('ˊ'), Some(1));
 }
