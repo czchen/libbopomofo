68334dde8ff225b73c305f8ba554ba2b5342e186
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2015-12-29 13:23:20 +0800

    Fix wrong value in tone

--- src/bopomofo.rs
+++ src/bopomofo.rs
@@ -9,8 +9,8 @@ fn convert_character_to_bopomofo(ch: char) -> Option<Bopomofo> {
     match ch {
         '˙' => Some(Bopomofo::Tone(1)),
         'ˊ' => Some(Bopomofo::Tone(2)),
-        'ˇ' => Some(Bopomofo::Tone(4)),
-        'ˋ' => Some(Bopomofo::Tone(5)),
+        'ˇ' => Some(Bopomofo::Tone(3)),
+        'ˋ' => Some(Bopomofo::Tone(4)),
         _ => None,
     }
 }
